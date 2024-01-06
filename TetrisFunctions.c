44c6a7f65dbc80942f4df8d63dbcc71c7e03566e
GiulioLV98 <giulioleoni5@gmail.com>
2024-01-06 09:24:58 +0100

    Order

--- TetrisFunctions.c
+++ TetrisFunctions.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include "TetrisFunctions.h"
 #include "TetrisDefine.h"
 
@@ -7,6 +6,39 @@ extern int score;
 extern float tetrominoDescentTime;
 extern Sound SFXDeleteLines;
 extern Color colorsTypes[9];
+
+void DrawTetromino(const int startOffsetX, const int startOffsetY, const int tetrominoCellX, const int tetrominoCellY, const int *tetromino, const Color tetrominoColor)
+{
+    for (int y = 0; y < TETROMINO_SIZE; y++)
+    {
+        for (int x = 0; x < TETROMINO_SIZE; x++)
+        {
+            const int offset = y * TETROMINO_SIZE + x;
+            if (tetromino[offset] == 1)
+            {
+                DrawRectangle((x + tetrominoCellX) * TILE_SIZE + startOffsetX, (y + tetrominoCellY) * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, tetrominoColor);
+            }
+        }
+    }
+}
+
+void DrawArena(const int startOffsetX, const int startOffsetY)
+{
+    for (int y = 0; y < STAGE_HEIGHT; y++)
+    {
+        for (int x = 0; x < STAGE_WIDTH; x++)
+        {
+            const int offset = y * STAGE_WIDTH + x;
+            const int stageColor = stage[offset]; 
+            if (stage[offset] != 0)
+            {
+                DrawRectangle(x * TILE_SIZE + startOffsetX, y * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, colorsTypes[stageColor - 1]);
+            }
+            
+            DrawRectangleLines(x * TILE_SIZE + startOffsetX, y * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, BLACK);
+        }
+    }
+}
 
 int CheckCollision(const int tetrominoCellX, const int tetrominoCellY, const int *tetromino)
 {
@@ -39,39 +71,6 @@ void UpdateScore()
     if ((score % DESCENT_SPEED_INCREASE_THRESHOLD) == 0 && tetrominoDescentTime > MAX_DESCENT_TIME)
     {
         tetrominoDescentTime -= 0.1;
-    }
-}
-
-void DrawTetromino(const int startOffsetX, const int startOffsetY, const int tetrominoCellX, const int tetrominoCellY, const int *tetromino, const Color tetrominoColor)
-{
-    for (int y = 0; y < TETROMINO_SIZE; y++)
-    {
-        for (int x = 0; x < TETROMINO_SIZE; x++)
-        {
-            const int offset = y * TETROMINO_SIZE + x;
-            if (tetromino[offset] == 1)
-            {
-                DrawRectangle((x + tetrominoCellX) * TILE_SIZE + startOffsetX, (y + tetrominoCellY) * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, tetrominoColor);
-            }
-        }
-    }
-}
-
-void DrawArena(const int startOffsetX, const int startOffsetY)
-{
-    for (int y = 0; y < STAGE_HEIGHT; y++)
-    {
-        for (int x = 0; x < STAGE_WIDTH; x++)
-        {
-            const int offset = y * STAGE_WIDTH + x;
-            const int stageColor = stage[offset]; 
-            if (stage[offset] != 0)
-            {
-                DrawRectangle(x * TILE_SIZE + startOffsetX, y * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, colorsTypes[stageColor - 1]);
-            }
-            
-            DrawRectangleLines(x * TILE_SIZE + startOffsetX, y * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, BLACK);
-        }
     }
 }
 
